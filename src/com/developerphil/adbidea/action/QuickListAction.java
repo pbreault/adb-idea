0b0c8f854d1de4e971b4ab17e0e517ff3b379cef
Philippe Breault <philippe.breault+git@gmail.com>
2015-05-18 09:51:54 -0400

    Quick access to all commands with the brand new "ADB Operations Popup..."

---
+++ src/com/developerphil/adbidea/action/QuickListAction.java
@@ -0,0 +1,44 @@
+package com.developerphil.adbidea.action;
+
+import com.intellij.ide.actions.QuickSwitchSchemeAction;
+import com.intellij.openapi.actionSystem.*;
+import com.intellij.openapi.project.DumbAware;
+import com.intellij.openapi.project.Project;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+public class QuickListAction extends QuickSwitchSchemeAction implements DumbAware {
+
+    protected void fillActions(@Nullable final Project project,
+                               @NotNull final DefaultActionGroup group,
+                               @NotNull final DataContext dataContext) {
+
+        if (project == null) {
+            return;
+        }
+
+        addAction("com.developerphil.adbidea.action.KillAction", group);
+        addAction("com.developerphil.adbidea.action.StartAction", group);
+        addAction("com.developerphil.adbidea.action.RestartAction", group);
+        addAction("com.developerphil.adbidea.action.ClearDataAction", group);
+        addAction("com.developerphil.adbidea.action.ClearDataAndRestartAction", group);
+    }
+
+    protected boolean isEnabled() {
+        return true;
+    }
+
+    private void addAction(final String actionId, final DefaultActionGroup toGroup) {
+        final AnAction action = ActionManager.getInstance().getAction(actionId);
+
+        // add action to group if it is available
+        if (action != null) {
+            toGroup.add(action);
+        }
+    }
+
+    protected String getPopupTitle(AnActionEvent e) {
+        return "ADB Operations Popup";
+    }
+
+}
