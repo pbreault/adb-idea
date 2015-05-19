ee21d3e95891d3cf28c2a6d985be28468995b237
Philippe Breault <philippe.breault+git@gmail.com>
2015-05-19 15:18:02 -0400

    Added the uninstall action to the quick list

--- src/com/developerphil/adbidea/action/QuickListAction.java
+++ src/com/developerphil/adbidea/action/QuickListAction.java
@@ -17,6 +17,7 @@ public class QuickListAction extends QuickSwitchSchemeAction implements DumbAwar
             return;
         }
 
+        addAction("com.developerphil.adbidea.action.UninstallAction", group);
         addAction("com.developerphil.adbidea.action.KillAction", group);
         addAction("com.developerphil.adbidea.action.StartAction", group);
         addAction("com.developerphil.adbidea.action.RestartAction", group);
