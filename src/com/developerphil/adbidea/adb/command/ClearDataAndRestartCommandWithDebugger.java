ea11d06e97197b617a5596d736404b39808d3749
draekko <draekko.software@gmail.com>
2015-09-10 20:03:17 -0400

    removed old directory path

--- src/com/developerphil/adbidea/adb/command/ClearDataAndRestartCommandWithDebugger.java
+++
@@ -1,7 +0,0 @@
-package com.developerphil.adbidea.adb.command;
-
-public class ClearDataAndRestartCommandWithDebugger extends CommandList {
-    public ClearDataAndRestartCommandWithDebugger() {
-        super(new ClearDataCommand(), new StartDefaultActivityCommandWithDebugger());
-    }
-}
--- src/com/developerphil/adbidea/adb/command/RestartPackageCommandWithDebugger.java
+++
@@ -1,8 +0,0 @@
-package com.developerphil.adbidea.adb.command;
-
-public class RestartPackageCommandWithDebugger extends CommandList {
-
-    public RestartPackageCommandWithDebugger() {
-        super(new KillCommand(), new StartDefaultActivityCommandWithDebugger());
-    }
-}
