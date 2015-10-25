7ab7516fd5391f70a6131a1538b9adf4102f6646
Philippe Breault <philippe.breault+git@gmail.com>
2015-10-25 18:33:28 -0400

    bugfix: java.lang.NoSuchMethodException on Android Studio 1.5 Preview 1

--- src/com/developerphil/adbidea/adb/AdbUtil.java
+++ src/com/developerphil/adbidea/adb/AdbUtil.java
@@ -4,13 +4,19 @@ import com.android.ddmlib.AdbCommandRejectedException;
 import com.android.ddmlib.IDevice;
 import com.android.ddmlib.ShellCommandUnresponsiveException;
 import com.android.ddmlib.TimeoutException;
+import com.android.tools.idea.model.ManifestInfo;
+import com.android.tools.idea.run.activity.DefaultActivityLocator;
 import com.developerphil.adbidea.adb.command.receiver.GenericReceiver;
+import org.jetbrains.android.dom.AndroidAttributeValue;
+import org.jetbrains.android.dom.manifest.UsesFeature;
 import org.jetbrains.android.facet.AndroidFacet;
-import org.jetbrains.android.run.DefaultActivityLocator;
 import org.jetbrains.android.util.AndroidUtils;
 import org.joor.Reflect;
+import org.joor.ReflectException;
 
 import java.io.IOException;
+import java.util.EnumSet;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class AdbUtil {
@@ -43,9 +49,47 @@ public class AdbUtil {
 
     public static String getDefaultLauncherActivityName(AndroidFacet facet) {
         try {
+            // Studio 1.5
             return DefaultActivityLocator.getDefaultLauncherActivityName(facet.getManifest());
         } catch (Error e) {
-            return Reflect.on(AndroidUtils.class).call("getDefaultLauncherActivityName", facet.getManifest()).get();
+            try {
+                // Studio 1.4
+                return Reflect.on("org.jetbrains.android.run.DefaultActivityLocator").call("getDefaultLauncherActivityName", facet.getManifest()).get();
+            } catch (ReflectException exception) {
+                // Studio 1.3 && Intellij 14.1
+                return Reflect.on(AndroidUtils.class).call("getDefaultLauncherActivityName", facet.getManifest()).get();
+            }
         }
+    }
+
+    // compatibility between studio 1.4 and 1.5-Preview1
+    public static EnumSet<IDevice.HardwareFeature> getRequiredHardwareFeatures(AndroidFacet facet) {
+        try {
+            if (com.android.tools.idea.run.LaunchUtils.isWatchFeatureRequired(facet)) {
+                return EnumSet.of(IDevice.HardwareFeature.WATCH);
+            } else {
+                return EnumSet.noneOf(IDevice.HardwareFeature.class);
+            }
+        } catch (Error e) {
+            ManifestInfo manifestInfo = ManifestInfo.get(facet.getModule(), true);
+            List<UsesFeature> requiredFeatures = Reflect.on(manifestInfo).call("getRequiredFeatures").get();
+            return getRequiredHardwareFeatures(requiredFeatures);
+        }
+    }
+
+    // Studio 1.4 and below
+    private static EnumSet<IDevice.HardwareFeature> getRequiredHardwareFeatures(List<UsesFeature> requiredFeatures) {
+        // Currently, this method is hardcoded to only search if the list of required features includes a watch.
+        // We may not want to search the device for every possible feature, but only a small subset of important
+        // features, starting with hardware type watch..
+
+        for (UsesFeature feature : requiredFeatures) {
+            AndroidAttributeValue<String> name = feature.getName();
+            if (name != null && UsesFeature.HARDWARE_TYPE_WATCH.equals(name.getStringValue())) {
+                return EnumSet.of(IDevice.HardwareFeature.WATCH);
+            }
+        }
+
+        return EnumSet.noneOf(IDevice.HardwareFeature.class);
     }
 }
--- src/com/developerphil/adbidea/ui/MyDeviceChooser.java
+++ src/com/developerphil/adbidea/ui/MyDeviceChooser.java
@@ -22,8 +22,8 @@ import com.android.sdklib.AndroidVersion;
 import com.android.sdklib.IAndroidTarget;
 import com.android.tools.idea.ddms.DeviceRenderer;
 import com.android.tools.idea.model.AndroidModuleInfo;
-import com.android.tools.idea.model.ManifestInfo;
 import com.android.tools.idea.run.LaunchCompatibility;
+import com.developerphil.adbidea.adb.AdbUtil;
 import com.intellij.openapi.Disposable;
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.application.ModalityState;
@@ -114,7 +114,7 @@ public class MyDeviceChooser implements Disposable {
     myFilter = filter;
     myMinSdkVersion = AndroidModuleInfo.get(facet).getRuntimeMinSdkVersion();
     myProjectTarget = projectTarget;
-    myRequiredHardwareFeatures = getRequiredHardwareFeatures(ManifestInfo.get(facet.getModule(), true).getRequiredFeatures());
+    myRequiredHardwareFeatures = AdbUtil.getRequiredHardwareFeatures(facet);
 
     myDeviceTable = new JBTable();
     myPanel = ScrollPaneFactory.createScrollPane(myDeviceTable);
