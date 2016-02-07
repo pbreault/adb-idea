f691f957ef4d5f6bdc8a25b6d2d69d22190d9f98
Philippe Breault <philippe.breault+git@gmail.com>
2016-02-07 10:09:54 -0500

    Fix device chooser on Android Studio 2.0 Preview 8

--- src/com/developerphil/adbidea/compatibility/CanRunOnDeviceCompatBeforeTwoDotZero.java
+++ src/com/developerphil/adbidea/compatibility/CanRunOnDeviceCompatBeforeTwoDotZero.java
@@ -9,14 +9,15 @@ import org.joor.Reflect;
 
 import java.util.EnumSet;
 
-public class CanRunOnDeviceCompat extends BackwardCompatibleGetter<LaunchCompatibility> {
+// Up until android studio 2.0 preview 8
+public class CanRunOnDeviceCompatBeforeTwoDotZero extends BackwardCompatibleGetter<LaunchCompatibility> {
 
     private final AndroidVersion myMinSdkVersion;
     private final IAndroidTarget myProjectTarget;
     private final EnumSet<IDevice.HardwareFeature> myRequiredHardwareFeatures;
     private final IDevice device;
 
-    public CanRunOnDeviceCompat(AndroidVersion myMinSdkVersion, IAndroidTarget myProjectTarget, EnumSet<IDevice.HardwareFeature> myRequiredHardwareFeatures, IDevice device) {
+    public CanRunOnDeviceCompatBeforeTwoDotZero(AndroidVersion myMinSdkVersion, IAndroidTarget myProjectTarget, EnumSet<IDevice.HardwareFeature> myRequiredHardwareFeatures, IDevice device) {
         this.myMinSdkVersion = myMinSdkVersion;
         this.myProjectTarget = myProjectTarget;
         this.myRequiredHardwareFeatures = myRequiredHardwareFeatures;
@@ -24,15 +25,15 @@ public class CanRunOnDeviceCompat extends BackwardCompatibleGetter<LaunchCompati
     }
 
     @Override
-    // Android Studio 2.0 Preview 8 +
+    // Android studio 1.5+
     protected LaunchCompatibility getCurrentImplementation() throws Throwable {
         AndroidDevice androidDevice = Reflect.on("com.android.tools.idea.run.ConnectedAndroidDevice").create(device, null).get();
-        return LaunchCompatibility.canRunOnDevice(myMinSdkVersion, myProjectTarget, myRequiredHardwareFeatures, androidDevice);
+        return Reflect.on(LaunchCompatibility.class).call("canRunOnDevice", myMinSdkVersion, myProjectTarget, myRequiredHardwareFeatures, androidDevice, null).get();
     }
 
     @Override
-    // Android Studio 2.0 Preview 7
+    // Android Studio 1.4-
     protected LaunchCompatibility getPreviousImplementation() {
-        return new CanRunOnDeviceCompatBeforeTwoDotZero(myMinSdkVersion, myProjectTarget, myRequiredHardwareFeatures, device).get();
+        return Reflect.on(LaunchCompatibility.class).call("canRunOnDevice", myMinSdkVersion, myProjectTarget, myRequiredHardwareFeatures, device, null).get();
     }
 }
