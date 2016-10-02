2a13c73b94b088451d492f70a71957fac357d811
Philippe Breault <philippe.breault+git@gmail.com>
2016-10-02 09:10:40 -0400

    Only show notifications for errors.

--- src/main/java/com/developerphil/adbidea/ui/NotificationHelper.java
+++ src/main/java/com/developerphil/adbidea/ui/NotificationHelper.java
@@ -1,26 +1,30 @@
 package com.developerphil.adbidea.ui;
 
-import com.intellij.notification.Notification;
+import com.intellij.notification.NotificationGroup;
+import com.intellij.notification.NotificationListener;
 import com.intellij.notification.NotificationType;
-import com.intellij.notification.Notifications;
 
 public class NotificationHelper {
+
+    private static final NotificationGroup INFO = NotificationGroup.logOnlyGroup("ADB Idea (Logging)");
+    private static final NotificationGroup ERRORS = NotificationGroup.balloonGroup("ADB Idea (Errors)");
+    private static final NotificationListener NOOP_LISTENER = (notification, event) -> {
+    };
+
     public static void info(String message) {
-        sendNotification(message, NotificationType.INFORMATION);
+        sendNotification(message, NotificationType.INFORMATION, INFO);
     }
 
     public static void error(String message) {
-        sendNotification(message, NotificationType.ERROR);
+        sendNotification(message, NotificationType.ERROR, ERRORS);
     }
 
-    public static void sendNotification(String message, NotificationType notificationType) {
-        Notification notification = new Notification("com.developerphil.adbidea", "ADB IDEA", espaceString(message), notificationType);
-        Notifications.Bus.notify(notification);
+    private static void sendNotification(String message, NotificationType notificationType, NotificationGroup notificationGroup) {
+        notificationGroup.createNotification("ADB IDEA", escapeString(message), notificationType, NOOP_LISTENER).notify(null);
     }
 
 
-    private static String espaceString(String string) {
-        // replace with both so that it returns are preserved in the notification ballon and in the event log
+    private static String escapeString(String string) {
         return string.replaceAll("\n", "\n<br />");
     }
 }
