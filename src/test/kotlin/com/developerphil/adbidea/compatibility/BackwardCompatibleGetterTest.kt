682dc7e1aa9da7faba0526e7d65074bf7ba4b8a6
Philippe Breault <philippe.breault@gmail.com>
2020-02-09 07:49:33 -0500

    Convert the `compatibility` package to Kotlin

--- src/test/kotlin/com/developerphil/adbidea/compatibility/BackwardCompatibleGetterTest.kt
+++ src/test/kotlin/com/developerphil/adbidea/compatibility/BackwardCompatibleGetterTest.kt
@@ -9,13 +9,13 @@ class BackwardCompatibleGetterTest {
     @Test
     fun onlyCallCurrentImplementationWhenItIsValid() {
         var value = false
-        object : BackwardCompatibleGetter<Boolean?>() {
-            override fun getCurrentImplementation(): Boolean? {
+        object : BackwardCompatibleGetter<Boolean>() {
+            override fun getCurrentImplementation(): Boolean {
                 value = true
                 return true
             }
 
-            override fun getPreviousImplementation(): Boolean? {
+            override fun getPreviousImplementation(): Boolean {
                 fail("should not be called")
                 return true
             }
@@ -34,26 +34,26 @@ class BackwardCompatibleGetterTest {
 
     @Test(expected = RuntimeException::class)
     fun throwExceptionsWhenTheyAreNotRelatedToBackwardCompatibility() {
-        object : BackwardCompatibleGetter<Boolean?>() {
-            override fun getCurrentImplementation(): Boolean? {
+        object : BackwardCompatibleGetter<Boolean>() {
+            override fun getCurrentImplementation(): Boolean {
                 throw RuntimeException("exception!")
             }
 
-            override fun getPreviousImplementation(): Boolean? {
+            override fun getPreviousImplementation(): Boolean {
                 fail("should not be called")
-                return true
+                return false
             }
         }.get()
     }
 
     private fun expectPreviousImplementationIsCalledFor(throwable: Throwable) {
         var value = false
-        object : BackwardCompatibleGetter<Boolean?>() {
-            override fun getCurrentImplementation(): Boolean? {
+        object : BackwardCompatibleGetter<Boolean>() {
+            override fun getCurrentImplementation(): Boolean {
                 throw throwable
             }
 
-            override fun getPreviousImplementation(): Boolean? {
+            override fun getPreviousImplementation(): Boolean {
                 value = true
                 return true
             }
