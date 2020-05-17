aa8a15e68ed1a347982b8b0452fd4160861d0003
Philippe Breault <philippe.breault@gmail.com>
2020-05-17 12:19:16 -0400

    Fix an issue where the debugger would fail to attach on Android Studio 3.6

--- src/main/kotlin/com/developerphil/adbidea/debugger/Debugger.kt
+++ src/main/kotlin/com/developerphil/adbidea/debugger/Debugger.kt
@@ -4,11 +4,13 @@ import com.android.ddmlib.Client
 import com.android.ddmlib.IDevice
 import com.android.tools.idea.run.AndroidProcessHandler
 import com.android.tools.idea.run.editor.AndroidDebugger
+import com.developerphil.adbidea.compatibility.BackwardCompatibleGetter
 import com.developerphil.adbidea.invokeLater
 import com.developerphil.adbidea.waitUntil
 import com.intellij.execution.ExecutionManager
 import com.intellij.execution.process.ProcessOutputTypes
 import com.intellij.openapi.project.Project
+import org.joor.Reflect.on
 
 class Debugger(private val project: Project, private val device: IDevice, private val packageName: String) {
 
@@ -26,18 +28,18 @@ class Debugger(private val project: Project, private val device: IDevice, privat
 
     private fun closeOldSessionAndRun(androidDebugger: AndroidDebugger<*>, client: Client) {
         terminateRunSessions(client)
-        androidDebugger.attachToClient(project, client)
+        AttachToClient(androidDebugger, project, client).get()
     }
 
     // Disconnect any active run sessions to the same client
     private fun terminateRunSessions(selectedClient: Client) {
-        val pid = selectedClient.clientData.pid
+        val pid = PidGetter(selectedClient).get()
 
         // find if there are any active run sessions to the same client, and terminate them if so
         for (handler in ExecutionManager.getInstance(project).getRunningProcesses()) {
             if (handler is AndroidProcessHandler) {
                 val client = handler.getClient(selectedClient.device)
-                if (client != null && client.clientData.pid == pid) {
+                if (client != null && PidGetter(client).get() == pid) {
                     handler.detachProcess()
                     handler.notifyTextAvailable("Disconnecting run session: a new debug session will be established.\n", ProcessOutputTypes.STDOUT)
                     break
@@ -45,6 +47,28 @@ class Debugger(private val project: Project, private val device: IDevice, privat
             }
         }
     }
+}
 
+/**
+ * To remove when 4.0 hits stable.
+ */
+class PidGetter(private val client: Client) : BackwardCompatibleGetter<Int>() {
+    override fun getCurrentImplementation() = client.clientData.pid
 
+    override fun getPreviousImplementation() = on(client).call("getClientData").call("getPid").get<Int>()!!
+}
+
+/**
+ * To remove when 4.0 hits stable.
+ */
+class AttachToClient(private val androidDebugger: AndroidDebugger<*>,
+                     private val project: Project,
+                     private val client: Client) : BackwardCompatibleGetter<Unit>() {
+    override fun getCurrentImplementation() {
+        androidDebugger.attachToClient(project, client, null)
+    }
+
+    override fun getPreviousImplementation() {
+        on(androidDebugger).call("attachToClient", project, client)
+    }
 }
