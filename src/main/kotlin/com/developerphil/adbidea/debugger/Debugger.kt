4676aa7289154959084220157c509858c234ebf9
Philippe Breault <philippe.breault@gmail.com>
2020-07-08 13:29:41 -0400

    Fix notifications and debugger on studio 4.0+

--- src/main/kotlin/com/developerphil/adbidea/debugger/Debugger.kt
+++ src/main/kotlin/com/developerphil/adbidea/debugger/Debugger.kt
@@ -6,8 +6,10 @@ import com.android.tools.idea.run.AndroidProcessHandler
 import com.android.tools.idea.run.editor.AndroidDebugger
 import com.developerphil.adbidea.compatibility.BackwardCompatibleGetter
 import com.developerphil.adbidea.invokeLater
+import com.developerphil.adbidea.on
 import com.developerphil.adbidea.waitUntil
 import com.intellij.execution.ExecutionManager
+import com.intellij.execution.process.ProcessHandler
 import com.intellij.execution.process.ProcessOutputTypes
 import com.intellij.openapi.project.Project
 import org.joor.Reflect.on
@@ -15,16 +17,18 @@ import org.joor.Reflect.on
 class Debugger(private val project: Project, private val device: IDevice, private val packageName: String) {
 
     fun attach() {
-        waitUntil { debuggerCanBeAttached() }
+        var client: Client? = null
+        waitUntil {
+            client = device.getClient(packageName)
+            AndroidDebugger.EP_NAME.extensions.isNotEmpty() && client != null
+        }
         for (androidDebugger in AndroidDebugger.EP_NAME.extensions) {
             if (androidDebugger.supportsProject(project)) {
-                invokeLater { closeOldSessionAndRun(androidDebugger, device.getClient(packageName)) }
+                invokeLater { closeOldSessionAndRun(androidDebugger, device.getClient(packageName) ?: client!!) }
                 break
             }
         }
     }
-
-    private fun debuggerCanBeAttached() = AndroidDebugger.EP_NAME.extensions.size != 0 && device.getClient(packageName) != null
 
     private fun closeOldSessionAndRun(androidDebugger: AndroidDebugger<*>, client: Client) {
         terminateRunSessions(client)
@@ -33,13 +37,21 @@ class Debugger(private val project: Project, private val device: IDevice, privat
 
     // Disconnect any active run sessions to the same client
     private fun terminateRunSessions(selectedClient: Client) {
-        val pid = PidGetter(selectedClient).get()
+        TerminateRunSession(selectedClient, project).get()
+    }
+}
 
+class TerminateRunSession(
+        private val selectedClient: Client,
+        private val project: Project
+) : BackwardCompatibleGetter<Unit>() {
+    override fun getCurrentImplementation() {
+        val pid = selectedClient.clientData.pid
         // find if there are any active run sessions to the same client, and terminate them if so
         for (handler in ExecutionManager.getInstance(project).getRunningProcesses()) {
             if (handler is AndroidProcessHandler) {
                 val client = handler.getClient(selectedClient.device)
-                if (client != null && PidGetter(client).get() == pid) {
+                if (client != null && client.clientData.pid == pid) {
                     handler.detachProcess()
                     handler.notifyTextAvailable("Disconnecting run session: a new debug session will be established.\n", ProcessOutputTypes.STDOUT)
                     break
@@ -47,20 +59,27 @@ class Debugger(private val project: Project, private val device: IDevice, privat
             }
         }
     }
+
+    override fun getPreviousImplementation() {
+        val pid = pidFrom(selectedClient)
+        // find if there are any active run sessions to the same client, and terminate them if so
+        for (handler in RunningProcessesGetter(project).get()) {
+            if (handler is AndroidProcessHandler) {
+                val device = on(selectedClient).call("getDevice").get<IDevice>()
+                val client = handler.getClient(device)
+                if (client != null && pidFrom(client) == pid) {
+                    handler.detachProcess()
+                    handler.notifyTextAvailable("Disconnecting run session: a new debug session will be established.\n", ProcessOutputTypes.STDOUT)
+                    break
+                }
+            }
+        }
+
+    }
+
+    private fun pidFrom(client: Client) = on(client).call("getClientData").call("getPid").get<Int>()!!
 }
 
-/**
- * To remove when 4.0 hits stable.
- */
-class PidGetter(private val client: Client) : BackwardCompatibleGetter<Int>() {
-    override fun getCurrentImplementation() = client.clientData.pid
-
-    override fun getPreviousImplementation() = on(client).call("getClientData").call("getPid").get<Int>()!!
-}
-
-/**
- * To remove when 4.0 hits stable.
- */
 class AttachToClient(private val androidDebugger: AndroidDebugger<*>,
                      private val project: Project,
                      private val client: Client) : BackwardCompatibleGetter<Unit>() {
@@ -70,5 +89,17 @@ class AttachToClient(private val androidDebugger: AndroidDebugger<*>,
 
     override fun getPreviousImplementation() {
         on(androidDebugger).call("attachToClient", project, client)
+    }
+}
+
+private class RunningProcessesGetter(
+        val project: Project
+) : BackwardCompatibleGetter<Array<ProcessHandler>>() {
+    override fun getCurrentImplementation(): Array<ProcessHandler> {
+        return ExecutionManager.getInstance(project).getRunningProcesses()
+    }
+
+    override fun getPreviousImplementation(): Array<ProcessHandler> {
+        return on<ExecutionManager>().call("getInstance", project).call("getRunningProcesses").get<Array<ProcessHandler>>()
     }
 }
--- src/main/kotlin/com/developerphil/adbidea/ui/NotificationHelper.kt
+++ src/main/kotlin/com/developerphil/adbidea/ui/NotificationHelper.kt
@@ -1,22 +1,19 @@
 package com.developerphil.adbidea.ui
 
-import com.intellij.notification.Notification
+import com.intellij.notification.NotificationDisplayType
 import com.intellij.notification.NotificationGroup
-import com.intellij.notification.NotificationListener
 import com.intellij.notification.NotificationType
-import javax.swing.event.HyperlinkEvent
 
 object NotificationHelper {
-    private val INFO = NotificationGroup.logOnlyGroup("ADB Idea (Logging)")
-    private val ERRORS = NotificationGroup.balloonGroup("ADB Idea (Errors)")
-    private val NOOP_LISTENER = NotificationListener { _: Notification?, _: HyperlinkEvent? -> }
+    private val INFO = NotificationGroup("ADB Idea (Logging)", NotificationDisplayType.NONE, true, null, null)
+    private val ERRORS = NotificationGroup("ADB Idea (Errors)", NotificationDisplayType.BALLOON, true, null, null)
 
     fun info(message: String) = sendNotification(message, NotificationType.INFORMATION, INFO)
 
     fun error(message: String) = sendNotification(message, NotificationType.ERROR, ERRORS)
 
     private fun sendNotification(message: String, notificationType: NotificationType, notificationGroup: NotificationGroup) {
-        notificationGroup.createNotification("ADB IDEA", escapeString(message), notificationType, NOOP_LISTENER).notify(null)
+        notificationGroup.createNotification("ADB IDEA", escapeString(message), notificationType, null).notify(null)
     }
 
     private fun escapeString(string: String) = string.replace("\n".toRegex(), "\n<br />")
